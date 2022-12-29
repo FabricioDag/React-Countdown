302ea50b5edf31ce1c8acf2a1aa24111b282ea7d
DagFabricio <115273639+FabricioDag@users.noreply.github.com>
2022-12-29 11:33:28 -0300

    Css Commit
    Some css added

--- countdown/src/components/Counter.js
+++ countdown/src/components/Counter.js
@@ -3,6 +3,10 @@ import React from 'react'
 const Counter = ({title,number}) => {
   return (
     <div className='counter'>
+        <div className='top-counter'>
+            <div className='left'></div>
+            <div className='right'></div>
+        </div>
         <p className='counter-number'>{number}</p>
         <p className='counter-text'>{title}</p>
     </div>
