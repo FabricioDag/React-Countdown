084574ec468ba76f1fad508b888529e4b30f2ed9
DagFabricio <115273639+FabricioDag@users.noreply.github.com>
2022-12-29 10:43:10 -0300

    Initial Commit

---
+++ countdown/src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = ({title,number}) => {
+  return (
+    <div className='counter'>
+        <p className='counter-number'>{number}</p>
+        <p className='counter-text'>{title}</p>
+    </div>
+  )
+}
+
+export default Counter
